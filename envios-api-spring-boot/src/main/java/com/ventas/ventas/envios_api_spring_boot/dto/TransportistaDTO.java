87c92ca80e551e7f5d9e8ed555c44f74de9bc327
Felipe Segovia <feli.segovia@duocuc.cl>
2025-06-10 01:37:45 -0400

    clases DTO terminadas

---
+++ envios-api-spring-boot/src/main/java/com/ventas/ventas/envios_api_spring_boot/dto/TransportistaDTO.java
@@ -0,0 +1,11 @@
+package com.ventas.ventas.envios_api_spring_boot.dto;
+
+import lombok.Data;
+
+@Data
+public class TransportistaDTO {
+
+    private Integer idTransportista;
+
+    private String nombre;
+}
---
+++ envios-api-spring-boot/src/main/java/com/ventas/ventas/envios_api_spring_boot/dto/VentaDTO.java
@@ -0,0 +1,16 @@
+package com.ventas.ventas.envios_api_spring_boot.dto;
+
+import java.time.LocalDate;
+
+
+import lombok.Data;
+
+@Data
+public class VentaDTO {
+    private Integer idVenta;
+    private Integer idCliente;
+    private Integer idVendedor;
+    private LocalDate fechaVenta;
+
+    
+}
